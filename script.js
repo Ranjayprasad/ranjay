f6691bf3d53478f6239ffe9a5c05574e4f94c0fd
Ranjayprasad <71914234+Ranjayprasad@users.noreply.github.com>
2026-07-22 22:06:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
